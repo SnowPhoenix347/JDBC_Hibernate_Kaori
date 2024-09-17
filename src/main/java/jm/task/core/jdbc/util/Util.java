03f87186df31de6790619525e5bcfdc8fa19d51b
snowphoenix347 <snowphoenix347@gmail.com>
2024-09-17 20:49:24 +0300

    Created connection to DB in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection;
+
+    public static Optional<Connection> getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        } catch (ReflectiveOperationException ex) {
+            System.err.println("ReflectiveOperationException: " + ex.getMessage());
+        }
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/kaoridb?" +
+                    "user=root&password=pass");
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+        }
+        return Optional.ofNullable(connection);
+    }
 }
